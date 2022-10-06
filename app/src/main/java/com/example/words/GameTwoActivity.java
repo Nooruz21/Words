486e23b78b60774eae0cc9b361e2ed45b050dc00
Nooruz21 <awbawb2129@gmail.com>
2022-10-06 15:07:03 +0600

    2

--- app/src/main/java/com/example/words/GameTwoActivity.java
+++ app/src/main/java/com/example/words/GameTwoActivity.java
@@ -11,17 +11,18 @@ import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.words.databinding.ActivityGameOneBinding;
+import com.example.words.databinding.ActivityGameTwoBinding;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
-public class GameOneActivity extends AppCompatActivity {
+public class GameTwoActivity extends AppCompatActivity {
 
-    private ActivityGameOneBinding binding;
+    private ActivityGameTwoBinding binding;
     private static final String CORRECT_WORD = "ПРОГРАММИСТ";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityGameOneBinding.inflate(getLayoutInflater());
+        binding = ActivityGameTwoBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         initImage();
         initListener();
@@ -32,13 +33,13 @@ public class GameOneActivity extends AppCompatActivity {
 
     private void initBtnCheck() {
         binding.btnCheck.setOnClickListener(view -> {
-            if (binding.edLetter.getText().toString().equals(CORRECT_WORD)) {
+            if (binding.etLetter.getText().toString().equals(CORRECT_WORD)) {
                 alertDialog();
-            } else if (binding.edLetter.getText().toString().equals("")) {
+            } else if (binding.etLetter.getText().toString().equals("")) {
                 Toast.makeText(this, "Поле пустое", Toast.LENGTH_SHORT).show();
             } else {
                 Toast.makeText(this, "Неверно!", Toast.LENGTH_SHORT).show();
-                binding.edLetter.setText("");
+                binding.etLetter.setText("");
                 binding.letter1.setVisibility(View.VISIBLE);
                 binding.letter2.setVisibility(View.VISIBLE);
                 binding.letter3.setVisibility(View.VISIBLE);
@@ -74,8 +75,8 @@ public class GameOneActivity extends AppCompatActivity {
 
     private void initClearLetter() {
         binding.btnClear.setOnClickListener(view -> {
-            String text = binding.edLetter.getText().toString();
-            binding.edLetter.setText(text.substring(0, text.length() - 1));
+            String text = binding.etLetter.getText().toString();
+            binding.etLetter.setText(text.substring(0, text.length() - 1));
             char letter = text.charAt(text.length() - 1);
 
             if (String.valueOf(letter).equals(binding.letter1.getText().toString())) {
@@ -120,7 +121,7 @@ public class GameOneActivity extends AppCompatActivity {
     }
 
     private void initBtnClear() {
-        binding.edLetter.addTextChangedListener(new TextWatcher() {
+        binding.etLetter.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
@@ -144,51 +145,51 @@ public class GameOneActivity extends AppCompatActivity {
 
     private void initListener() {
         binding.letter1.setOnClickListener(view -> {
-            binding.edLetter.append(binding.letter1.getText().toString());
+            binding.etLetter.append(binding.letter1.getText().toString());
             binding.letter1.setVisibility(View.INVISIBLE);
         });
         binding.letter2.setOnClickListener(view -> {
-            binding.edLetter.append(binding.letter2.getText().toString());
+            binding.etLetter.append(binding.letter2.getText().toString());
             binding.letter2.setVisibility(View.INVISIBLE);
         });
         binding.letter3.setOnClickListener(view -> {
-            binding.edLetter.append(binding.letter3.getText().toString());
+            binding.etLetter.append(binding.letter3.getText().toString());
             binding.letter3.setVisibility(View.INVISIBLE);
         });
         binding.letter4.setOnClickListener(view -> {
-            binding.edLetter.append(binding.letter4.getText().toString());
+            binding.etLetter.append(binding.letter4.getText().toString());
             binding.letter4.setVisibility(View.INVISIBLE);
         });
         binding.letter5.setOnClickListener(view -> {
-            binding.edLetter.append(binding.letter5.getText().toString());
+            binding.etLetter.append(binding.letter5.getText().toString());
             binding.letter5.setVisibility(View.INVISIBLE);
         });
         binding.letter6.setOnClickListener(view -> {
-            binding.edLetter.append(binding.letter6.getText().toString());
+            binding.etLetter.append(binding.letter6.getText().toString());
             binding.letter6.setVisibility(View.INVISIBLE);
         });
         binding.letter7.setOnClickListener(view -> {
-            binding.edLetter.append(binding.letter7.getText().toString());
+            binding.etLetter.append(binding.letter7.getText().toString());
             binding.letter7.setVisibility(View.INVISIBLE);
         });
         binding.letter8.setOnClickListener(view -> {
-            binding.edLetter.append(binding.letter8.getText().toString());
+            binding.etLetter.append(binding.letter8.getText().toString());
             binding.letter8.setVisibility(View.INVISIBLE);
         });
         binding.letter9.setOnClickListener(view -> {
-            binding.edLetter.append(binding.letter9.getText().toString());
+            binding.etLetter.append(binding.letter9.getText().toString());
             binding.letter9.setVisibility(View.INVISIBLE);
         });
         binding.letter10.setOnClickListener(view -> {
-            binding.edLetter.append(binding.letter10.getText().toString());
+            binding.etLetter.append(binding.letter10.getText().toString());
             binding.letter10.setVisibility(View.INVISIBLE);
         });
         binding.letter11.setOnClickListener(view -> {
-            binding.edLetter.append(binding.letter11.getText().toString());
+            binding.etLetter.append(binding.letter11.getText().toString());
             binding.letter11.setVisibility(View.INVISIBLE);
         });
         binding.letter12.setOnClickListener(view -> {
-            binding.edLetter.append(binding.letter12.getText().toString());
+            binding.etLetter.append(binding.letter12.getText().toString());
             binding.letter12.setVisibility(View.INVISIBLE);
         });
     }
@@ -199,7 +200,6 @@ public class GameOneActivity extends AppCompatActivity {
         Glide.with(this).load("https://rsv.ru/blog/wp-content/uploads/2021/09/programmist-918x516.jpg").centerCrop().into(binding.imageTwo);
         Glide.with(this).load("https://rsv.ru/blog/wp-content/uploads/2021/09/programmist-918x516.jpg").into(binding.imageThird);
         Glide.with(this).load("https://rsv.ru/blog/wp-content/uploads/2021/09/programmist-918x516.jpg").into(binding.imageFour);
-
 
     }
 }
